b0ed924e58d83ee5864740da0c80738193bddeee
R_N101 <R_N101@msn.com>
2018-08-31 19:40:41 +0100

    filter user ratings working alone, need to connect with other filters

--- src/components/component.js
+++ src/components/component.js
@@ -1,274 +1,314 @@
-import React, { Component } from 'react';
-import {Card, Col, Row, Preloader} from 'react-materialize';
-import SortBy from './sortBy';
-import FilterName from './filterName';
-import FilterStars from './filterStars';
-import FilterUserRatings from './filterUserRatings';
-import FilterMinCost from './filterMinCost';
+import React, { Component } from "react";
+import { Card, Col, Row, Preloader } from "react-materialize";
+import SortBy from "./sortBy";
+import FilterName from "./filterName";
+import FilterStars from "./filterStars";
+import FilterUserRatings from "./filterUserRatings";
+import FilterMinCost from "./filterMinCost";
 
-import ResetButton from './resetButton';
+import ResetButton from "./resetButton";
 
 let initialState = {};
 
 class Client extends Component {
+  /* constructor - deprecated with React 16 */
+  // constructor() {
+  //     super();
+  //     this.state = {
+  //         hotels: [],
+  //         sortBy: '',
+  //         filterName: '',
+  //         filterStars: ''
+  //     }
+  // }
 
-    /* constructor - deprecated with React 16 */
-    // constructor() {
-    //     super();
-    //     this.state = {
-    //         hotels: [], 
-    //         sortBy: '',
-    //         filterName: '',
-    //         filterStars: ''
-    //     }
-    // }
+  state = {
+    hotels: [],
+    sortBy: "",
+    filterName: "",
+    filterStars: "",
+    filterUserRating: "",
+    filterMinCost: ""
+  };
 
-    state = {
-        hotels: [], 
-        sortBy: '',
-        filterName: '',
-        filterStars: '',
-        filterUserRating: '',
-        filterMinCost: ''
-    }
+  apidata = () => {
+    window.location.href.includes("localhost")
+      ? console.log("localhost")
+      : console.log("not localhost");
 
+    let dataSource;
 
-    apidata = () => {
+    window.location.href.includes("localhost")
+      ? (dataSource = "/api/") //development backend server url
+      : (dataSource = "https://travel-app-frontend-rn.herokuapp.com/api/"); //heroku backend server url
 
-        window.location.href.includes("localhost")
-        ? console.log("localhost")
-        : console.log("not localhost")
-
-        let dataSource;
-
-        window.location.href.includes("localhost")
-        ? dataSource = '/api/' //development backend server url
-        : dataSource = 'https://travel-app-frontend-rn.herokuapp.com/api/' //heroku backend server url
-        
-        fetch(dataSource)
-        .then(res => res.json()
-        .then(data => {
-            const hotelData = data.Establishments;
-            this.setState({
-                ...this.state,
-                hotels:hotelData
-            });
-
-            // copy of the original data from fetch
-            initialState = {...this.state};
-        }));
-    };
-
-    componentDidMount() {
-        this.apidata();
-    }
-
-    handleSort = (event) => {
-
+    fetch(dataSource).then(res =>
+      res.json().then(data => {
+        const hotelData = data.Establishments;
         this.setState({
-            sortBy: event.target.value
+          ...this.state,
+          hotels: hotelData
         });
-        let newObj = [...this.state.hotels];
-        
-        switch(this.state.sortBy) {
-            case("1"):
-                newObj.sort((a, b) => a.Distance - b.Distance);
-                this.setState({
-                    ...this.state,
-                    hotels: newObj
-                });
-                break;
-            
-            case("2"):
-                newObj.sort((a, b) => b.Distance - a.Distance);
-                this.setState({
-                    ...this.state,
-                    hotels: newObj
-                });
-                break;
-            
-            case("3"):
-                newObj.sort((a, b) => a.Stars - b.Stars);
-                this.setState({
-                    ...this.state,
-                    hotels: newObj
-                });
-                break;
-                
-            case("4"):
-                newObj.sort((a, b) => b.Stars - a.Stars);
-                this.setState({
-                    ...this.state,
-                    hotels: newObj
-                });
-                break;
-            
-            case("5"):
-                newObj.sort((a, b) => a.MinCost - b.MinCost);
-                this.setState({
-                    ...this.state,
-                    hotels: newObj
-                });
-                break;
 
-            case("6"):
-                newObj.sort((a, b) => b.MinCost - a.MinCost);
-                this.setState({
-                    ...this.state,
-                    hotels: newObj
-                });
-                break;
+        // copy of the original data from fetch
+        initialState = { ...this.state };
+      })
+    );
+  };
 
-            case("7"):
-                newObj.sort((a, b) => a.UserRating - b.UserRating);
-                this.setState({
-                    ...this.state,
-                    hotels: newObj
-                });
-                break;
+  componentDidMount() {
+    this.apidata();
+  }
 
-            case("8"):
-                newObj.sort((a, b) => b.UserRating - a.UserRating);
-                this.setState({
-                    ...this.state,
-                    hotels: newObj
-                });
-                break;
-            
-            default:
-                this.setState({
-                    ...this.state,
-                    hotels: initialState.hotels
-                });
-        }
+  handleSort = event => {
+    this.setState({
+      sortBy: event.target.value
+    });
+    let newObj = [...this.state.hotels];
 
-    }
-
-    resetData = () => {
+    switch (this.state.sortBy) {
+      case "1":
+        newObj.sort((a, b) => a.Distance - b.Distance);
         this.setState({
-            filterName: ''
+          ...this.state,
+          hotels: newObj
         });
-        document.querySelector(".filterName").value="";
-    }
+        break;
 
-    handleFilterName = (event) => {
-        let nameInput = event.target.value;
+      case "2":
+        newObj.sort((a, b) => b.Distance - a.Distance);
         this.setState({
-            filterName: nameInput
+          ...this.state,
+          hotels: newObj
         });
-                        
-    };
+        break;
 
-    handleFilterStars = (event) => {
-        let starInput = parseInt(document.querySelector(".filterStars input").value, 10);
-        console.log("starInput: ",starInput, typeof(starInput));
+      case "3":
+        newObj.sort((a, b) => a.Stars - b.Stars);
         this.setState({
-            filterStars: starInput
-        })
-    };
+          ...this.state,
+          hotels: newObj
+        });
+        break;
 
-    
+      case "4":
+        newObj.sort((a, b) => b.Stars - a.Stars);
+        this.setState({
+          ...this.state,
+          hotels: newObj
+        });
+        break;
+
+      case "5":
+        newObj.sort((a, b) => a.MinCost - b.MinCost);
+        this.setState({
+          ...this.state,
+          hotels: newObj
+        });
+        break;
+
+      case "6":
+        newObj.sort((a, b) => b.MinCost - a.MinCost);
+        this.setState({
+          ...this.state,
+          hotels: newObj
+        });
+        break;
+
+      case "7":
+        newObj.sort((a, b) => a.UserRating - b.UserRating);
+        this.setState({
+          ...this.state,
+          hotels: newObj
+        });
+        break;
+
+      case "8":
+        newObj.sort((a, b) => b.UserRating - a.UserRating);
+        this.setState({
+          ...this.state,
+          hotels: newObj
+        });
+        break;
+
+      default:
+        this.setState({
+          ...this.state,
+          hotels: initialState.hotels
+        });
+    }
+  };
+
+  resetData = () => {
+    this.setState({
+      filterName: ""
+    });
+    document.querySelector(".filterName").value = "";
+  };
+
+  handleFilterName = event => {
+    let nameInput = event.target.value;
+    this.setState({
+      filterName: nameInput
+    });
+  };
+
+  handleFilterStars = () => {
+    let starInput = parseInt(
+      document.querySelector(".filterStars input").value,
+      10
+    );
+    console.log("starInput: ", starInput, typeof starInput);
+    this.setState({
+      filterStars: starInput
+    });
+  };
+
+  handleFilterRatings = () => {
+    let userRatingInput = parseInt(
+      document.querySelector(".filterUserRatings input").value,
+      10
+    );
+    this.setState({
+      filterUserRating: userRatingInput
+    });
+    console.log("hi ", userRatingInput);
+  };
+
   render() {
+    // if (this.state.hotels.length) {
+    console.log("load");
 
-   // if (this.state.hotels.length) {
-        console.log("load");
+    const { hotels } = this.state;
 
-        const {hotels} = this.state;
+    let hotelFilterStars = [];
+    let hotelFilterUserRatings = [];
+    let hotelsToRender = [];
 
-        let hotelFilterStars = [];
-        let hotelsToRender = [];
+    let filterStarsLength = this.state.filterStars.length !== 0;
 
-        let filterStarsLength = this.state.filterStars.length !== 0;        
-        
-        filterStarsLength   
-            ? hotels.filter( hotel => {
-                hotel.Stars === this.state.filterStars 
-                    ? hotelFilterStars.push(hotel)
-                    : []
-            })
-            : []
-            
-        console.log("hotelFilterStars: ",hotelFilterStars.length, hotelFilterStars);
+    let filterUserRatingsLength = this.state.filterUserRating.length !== 0;
 
-        
-        hotelFilterStars.length > 0
-            ? hotelsToRender = hotelFilterStars.filter(hotel => 
-                hotel.Name.toLowerCase().includes(
-                    this.state.filterName.toLowerCase())
-            )
-            : hotelsToRender = hotels.filter(hotel => 
-                hotel.Name.toLowerCase().includes(
-                    this.state.filterName.toLowerCase())
-            )
+    filterStarsLength
+      ? hotels.filter(hotel => {
+          hotel.Stars === this.state.filterStars
+            ? hotelFilterStars.push(hotel)
+            : [];
+        })
+      : [];
 
+    filterUserRatingsLength
+      ? hotels.filter(hotel => {
+          hotel.UserRating === this.state.filterUserRating
+            ? hotelFilterUserRatings.push(hotel)
+            : [];
+        })
+      : [];
 
-        return (
-            
-            <div className="client">
-                
-                {this.state.hotels.length 
-                    ? <div className="container">
+    console.log(
+      "hotelFilterStars: ",
+      hotelFilterStars.length,
+      hotelFilterStars
+    );
 
-                            <Row>
-                                <SortBy handleSort={this.handleSort} />
-                            </Row>
+    hotelFilterStars.length > 0
+      ? (hotelsToRender = hotelFilterStars.filter(hotel =>
+          hotel.Name.toLowerCase().includes(this.state.filterName.toLowerCase())
+        ))
+      : (hotelsToRender = hotels.filter(hotel =>
+          hotel.Name.toLowerCase().includes(this.state.filterName.toLowerCase())
+        ));
 
-                            <Row>
-                                <FilterName handleFilterName={this.handleFilterName} />
-                            </Row>
+    hotelFilterUserRatings.length > 0
+      ? (hotelsToRender = hotelFilterUserRatings.filter(hotel =>
+          hotel.Name.toLowerCase().includes(this.state.filterName.toLowerCase())
+        ))
+      : (hotelsToRender = hotels.filter(hotel =>
+          hotel.Name.toLowerCase().includes(this.state.filterName.toLowerCase())
+        ));
 
-                            <Row>
-                                <FilterStars handleFilterStars={this.handleFilterStars}/>
-                                <FilterUserRatings />
-                                <FilterMinCost />
-                            </Row>
+    return (
+      <div className="client">
+        {this.state.hotels.length ? (
+          <div className="container">
+            <Row>
+              <SortBy handleSort={this.handleSort} />
+            </Row>
 
-                            <Row>
-                                <ResetButton resetData={this.resetData} />
-                            </Row>
+            <Row>
+              <FilterName handleFilterName={this.handleFilterName} />
+            </Row>
 
-                            <Row>
-                                    
-                                { hotelsToRender.map(hotel =>
-                                
-                                    <Col m={6} s={12} key={hotel.EstablishmentId}>
-                                        <Card>
-                                            <div className="card-image">
-                                                <img src={hotel.ImageUrl} alt="" />
-                                                <span className="card-title">{hotel.Name}</span>
-                                            </div>
-                                            <div className="card-content">
-                                                <p><b>Type: </b>{hotel.EstablishmentType}</p>
-                                                <p><b>Location: </b>{hotel.Location}</p>
-                                                <p><b>Minimum cost: </b>£{hotel.MinCost}</p>
-                                                <p><b>Stars: </b>{hotel.Stars}</p>
-                                                <p><b>User rating: </b>{hotel.UserRating}</p>
-                                                <p><b>User rating title: </b>{hotel.UserRatingTitle}</p>
-                                                <p><b>User rating count: </b>{hotel.UserRatingCount}</p>
-                                                <p><b>Distance: </b>{parseFloat(hotel.Distance).toFixed(2)}</p>
-                                            </div>
-                                        </Card>
-                                    </Col>
-                                    
-                                )}
+            <Row>
+              <FilterStars handleFilterStars={this.handleFilterStars} />
+              <FilterUserRatings
+                handleFilterRatings={this.handleFilterRatings}
+              />
+              <FilterMinCost />
+            </Row>
 
-                            </Row>
-                        </div>
+            <Row>
+              <ResetButton resetData={this.resetData} />
+            </Row>
 
-                    :  <div className="container">
-                            <Row>
-                                <Col s={12}>
-                                    <Preloader flashing size='big'/>
-                                </Col>
-                            </Row> 
-                        </div>
-                    }
-
-            </div>
-
-        );
-
-    }
+            <Row>
+              {hotelsToRender.map(hotel => (
+                <Col m={6} s={12} key={hotel.EstablishmentId}>
+                  <Card>
+                    <div className="card-image">
+                      <img src={hotel.ImageUrl} alt="" />
+                      <span className="card-title">{hotel.Name}</span>
+                    </div>
+                    <div className="card-content">
+                      <p>
+                        <b>Type: </b>
+                        {hotel.EstablishmentType}
+                      </p>
+                      <p>
+                        <b>Location: </b>
+                        {hotel.Location}
+                      </p>
+                      <p>
+                        <b>Minimum cost: </b>£{hotel.MinCost}
+                      </p>
+                      <p>
+                        <b>Stars: </b>
+                        {hotel.Stars}
+                      </p>
+                      <p>
+                        <b>User rating: </b>
+                        {hotel.UserRating}
+                      </p>
+                      <p>
+                        <b>User rating title: </b>
+                        {hotel.UserRatingTitle}
+                      </p>
+                      <p>
+                        <b>User rating count: </b>
+                        {hotel.UserRatingCount}
+                      </p>
+                      <p>
+                        <b>Distance: </b>
+                        {parseFloat(hotel.Distance).toFixed(2)}
+                      </p>
+                    </div>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </div>
+        ) : (
+          <div className="container">
+            <Row>
+              <Col s={12}>
+                <Preloader flashing size="big" />
+              </Col>
+            </Row>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Client;
--- src/components/filterUserRatings.js
+++ src/components/filterUserRatings.js
@@ -5,12 +5,17 @@ const FilterUserRatings = props => (
     
     <Input className="filterUserRatings" s={12} m={4} type='select' defaultValue='' onChange={props.handleFilterRatings} >
         <option key="" value="">Filter User Ratings:</option>
-        <option key="0" value="0">0 Star</option>
-        <option key="1" value="1">1 Star</option>
-        <option key="2" value="2">2 Stars</option>
-        <option key="3" value="3">3 Stars</option>
-        <option key="4" value="4">4 Stars</option>
-        <option key="5" value="5">5 Stars</option>
+        <option key="0" value="0">0</option>
+        <option key="1" value="1">1</option>
+        <option key="2" value="2">2</option>
+        <option key="3" value="3">3</option>
+        <option key="4" value="4">4</option>
+        <option key="5" value="5">5</option>
+        <option key="6" value="6">6</option>
+        <option key="7" value="7">7</option>
+        <option key="8" value="8">8</option>
+        <option key="9" value="9">9</option>
+        <option key="10" value="10">10</option>
     </Input>
 
 );
